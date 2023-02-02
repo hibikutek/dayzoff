0a0d675b826370211f77380e051e0d122a6863ec
Jerry <jerry.causby@telesoftas.com>
2023-02-02 10:34:39 +0200

    initial commit

---
+++ src/main/java/com/hibikutek/dayzoff/DayzOffApplication.java
@@ -0,0 +1,13 @@
+package com.hibikutek.dayzoff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DayzOffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DayzOffApplication.class, args);
+	}
+
+}
